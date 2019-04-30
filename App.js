0e258b08d0ded7ab35fb5af2802040ec7dc391ef
damian <damcho@gmail.com>
2019-04-29 21:54:55 -0300

    app structure refactor

--- App.js
+++ App.js
@@ -5,10 +5,10 @@ import {
   createSwitchNavigator
 } from "react-navigation";
 
-import CocktailsListContainer from "./cocktailsModule/components/cocktailsList/CocktailsListContainer";
-import CocktailDetailContainer from "./cocktailsModule/components/cocktailDetail/CocktailDetailContainer";
-import CreateCocktailFormContainer from "./cocktailsModule/components/createCocktail/CreateCocktailFormContainer";
-import LoginScreenContainer from "./loginModule/components/loginScreen/LoginScreenContainer";
+import CocktailsListContainer from "./appModules/cocktailsModule/components/cocktailsList/CocktailsListContainer";
+import CocktailDetailContainer from "./appModules/cocktailsModule/components/cocktailDetail/CocktailDetailContainer";
+import CreateCocktailFormContainer from "./appModules/cocktailsModule/components/createCocktail/CreateCocktailFormContainer";
+import LoginScreenContainer from "./appModules/loginModule/components/loginScreen/LoginScreenContainer";
 
 import { Provider } from "react-redux";
 import { combineReducers } from "redux";
@@ -16,8 +16,8 @@ import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
 import { createStore, applyMiddleware } from "redux";
 
-import cocktailsRootReducer from "./cocktailsModule/reducers/cocktailsModuleReducers";
-import user from "./loginModule/reducers/loginModuleReducers";
+import cocktailsRootReducer from "./appModules/cocktailsModule/reducers/cocktailsModuleReducers";
+import user from "./appModules/loginModule/reducers/loginModuleReducers";
 
 console.disableYellowBox = true;
 
