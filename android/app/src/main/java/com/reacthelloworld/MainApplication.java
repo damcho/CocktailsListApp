19818f61343c95b513ef52142a6edf9aef6ea6e6
damian <damcho@gmail.com>
2019-05-06 19:46:21 -0300

    Android configuration files

--- android/app/src/main/java/com/reacthelloworld/MainApplication.java
+++ android/app/src/main/java/com/reacthelloworld/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,8 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
+            new MainReactPackage(),
             new RNFirebasePackage(),
+            new RNFirebaseAuthPackage(),
             new ImagePickerPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage()
